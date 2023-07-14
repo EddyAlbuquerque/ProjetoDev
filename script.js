ee6d6dd02c3f68e5c5567fb9acc647de101e6d9f
Eddy Albuquerque <eddy.albuquerque@hotmail.com>
2023-07-13 22:17:18 -0300

    Correções

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  // trocar img
+  // pegar a tag img
   const img = document.querySelector("#profile img")
+
+  // substituir a imagem
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "/assets/avatar-light.png")
-    img.setAttribute("alt", "foto de Edinho com oculos fundo vermelho")
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    img.setAttribute("src", "/assets/avatar.png")
-    img.setAttribute("alt", "foto de Edinho com oculos fundo branco")
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
